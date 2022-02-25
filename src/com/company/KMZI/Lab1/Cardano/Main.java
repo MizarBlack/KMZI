9918446e211acaab650c95aa6a9590b98e22d5d3
MizarBlack <Gromotey2000@yandex.ru>
2022-02-25 15:36:54 +0400

    КМЗИ 5 вариант.

--- src/com/company/KMZI/Lab1/Cardano/Main.java
+++ src/com/company/KMZI/Lab1/Cardano/Main.java
@@ -6,11 +6,11 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static char [][] rotate(char[][] def, int size ){
-        char [][] chars = new char[size][size];
-        for(int i = 0; i < size; i++){
+    public static char[][] rotate(char[][] def, int size) {
+        char[][] chars = new char[size][size];
+        for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                chars[j][size-1-i] = def[i][j];
+                chars[j][size - 1 - i] = def[i][j];
             }
         }
         return chars;
@@ -22,65 +22,68 @@ public class Main {
         String keyFile = "C:\\Users\\Gromotey\\IdeaProjects\\untitled1\\input.txt";
 
 
-
         File inFile = new File(keyFile); // Чтение ключа в массив char
         Scanner scanner = new Scanner(inFile);
-        char [][] key = new char[sizeTable][sizeTable];
-        for(int i = 0; i < sizeTable; i++) {
+        char[][] key = new char[sizeTable][sizeTable];
+        for (int i = 0; i < sizeTable; i++) {
             key[i] = scanner.nextLine().toCharArray();
         }
-        /*for(int i = 0; i < sizeTable; i++){
-            for (int j = 0; j < sizeTable; j++){
-                System.out.print(key[i][j]);
-            }
-            System.out.println();
-        }*/
 
-        System.out.println("Input message: ");  // Ввод сообщения
+        System.out.print("Input message for encryption: ");  // Ввод сообщения
         Scanner scannerChar = new Scanner(System.in);
         String message = scannerChar.nextLine();
-        char [] messageChar = new char[0];
+        char[] messageChar = new char[0];
         if (message.length() <= maxSizeMessage) {
             messageChar = message.toCharArray();
-            //System.out.println(messageChar);
-        }
-        else {
-            System.out.println("Сообщение слишком большое для данной решётки. Введите сообщение не больше 64 символов.");
+        } else {
+            System.out.println("\nСообщение слишком большое для данной решётки.");
         }
 
-
-        char [][] anSecretMessage = new char[sizeTable][sizeTable];
-        for (int i = 0; i < sizeTable; i++){
-            for (int j = 1; j <= sizeTable; j++){
-                anSecretMessage[i][j-1] = messageChar[j + (i * 8 - 1)];
-            }
-        }
-
-        /*for (int i = 0; i < sizeTable; i++){
-            for (int j = 0; j < sizeTable; j++){
-                System.out.print(key[i][j]);
-            }
-            System.out.println();
-        }*/
-
-        char [][] secretMessage = new char[sizeTable][sizeTable]; //Текст для шифрования по лабораторной КМЗИ.Сделал Громов Владимир
-        for (int q = 0; q < 4; q++){ // Повторяем четыре раза, на четыре поворота ключа
-            for (int i = 0; i < sizeTable; i++){
-                for (int j = 0; j < sizeTable; j++){
-                    if (key[i][j] == 49){
-                        secretMessage[i][j] = anSecretMessage[i][j];
+        char[][] secretMessage = new char[sizeTable][sizeTable]; //Текст для шифрования по лабораторной КМЗИ.Сделал Громов Владимир
+        int count = 0;
+        for (int q = 0; q < 4; q++) { // Повторяем четыре раза, на четыре поворота ключа
+            for (int i = 0; i < sizeTable; i++) {
+                for (int j = 0; j < sizeTable; j++) {
+                    if (key[i][j] == '1') {
+                        secretMessage[i][j] = messageChar[count];
+                        count++;
                     }
                 }
             }
             key = rotate(key, sizeTable);
         }
 
-        for (int i = 0; i < sizeTable; i++){
-            for (int j = 0; j < sizeTable; j++){
-                System.out.print(secretMessage[i][j]);
+        for (int i = 0; i < sizeTable; i++) {
+            for (int j = 0; j < sizeTable; j++) {
+                    System.out.print(secretMessage[i][j]);
             }
-            System.out.println();
         }
+
+        System.out.print("\nInput message for decryption: ");  // Ввод сообщения
+        message = scannerChar.nextLine();
+        messageChar = new char[0];
+        if (message.length() <= maxSizeMessage) {
+            messageChar = message.toCharArray();
+        } else {
+            System.out.println("Сообщение слишком большое для данной решётки.");
+        }
+
+        count = 0; //Тр енаониГя пксртой  мКдоловя М В лЗабИл.шоСифрдаеладитмирроаовл
+        for (int q = 0; q < 4; q++) { // Повторяем четыре раза, на четыре поворота ключа
+            for (int i = 0; i < sizeTable; i++) {
+                for (int j = 0; j < sizeTable; j++) {
+                    if (key[i][j] == '1') {
+                        messageChar[count] = secretMessage[i][j];
+                        count++;
+                    }
+                }
+            }
+            key = rotate(key, sizeTable);
+        }
+
+            for (int j = 0; j < maxSizeMessage; j++){
+                System.out.print(messageChar[j]);
+            }
 
     }
 }
